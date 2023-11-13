ea47e1563d61b8914752a990b02fb954dae198aa
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2023-11-13 12:33:22 +0800

    backup

--- code/arma.py
+++ code/arma.py
@@ -13,7 +13,7 @@ data = [75, 72, 70, 68, 75, 80, 82, 78, 80, 76, 78, 75, 80, 85,
        90, 92, 95, 100, 98, 96, 92, 88, 85, 82, 80, 78, 75]
 
 
-# 预测(dta,model,p,q=1,L=1,k=39)
+# 预测(dta,model,p,q=1,k=39)
 print('the len of data:', len(data))
 ARMA_pre, ARMA_pre_inv = pre.interface_pre(data, 3, 15, 1, 20)
 print('predict data:', ARMA_pre)
