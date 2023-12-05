a26c0a04c908f1b63bc70eb373246aa60a328e49
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2023-12-05 15:55:47 +0800

    init pulse rate sensor

--- code/sensors_control.py
+++ code/sensors_control.py
@@ -1,11 +1,27 @@
 import time
 import LCD1602 as LCD
+from pulse_sensor import PulseSensor
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)      # Ignore warning for now
 
 if __name__ == '__main__':
     LCD.init_lcd()
     time.sleep(1)
 
+    p = PulseSensor()
+    p.startAsyncBPM()
+
     while True:
+        bpm = p.BPM
+        if(bpm-125) > 0:
+            #GPIO.output(12, GPIO.HIGH)
+            LCD.print_lcd(1, 1, bpm - 125)
+            print("HIGH" % (bpm - 125))
+        else:
+            print("no heart beat")
+        
         #now = time.strftime('%m/%d %H:%M:%S', time.localtime(time.time()))
-        LCD.print_lcd(1, 1, 'Hello, world!')
+        #LCD.print_lcd(1, 1, 'Hello, world!')
         time.sleep(0.2)
