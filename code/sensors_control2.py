6a6d5893d0c2478e9f9eae0062f612bcf7861956
Liu <SWE2009510@xmu.edu.my>
2023-12-07 16:17:13 +0800

    sensorv2

---
+++ code/sensors_control2.py
@@ -0,0 +1,169 @@
+import time
+import LCD1602 as LCD
+from pulsesensor import PulseSensor
+import RPi.GPIO as GPIO
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+from scipy import signal,stats
+import multiprocessing
+import pandas as pd
+
+import sys
+import time
+import smbus
+import serial
+import regex as re
+
+import LCD1602 as LCD
+import RPi.GPIO as GPIO
+
+from web3 import Web3
+
+from hl7apy.core import Message
+
+from cryptography.fernet import Fernet
+
+key = b'SfMxJjQQBBZ1DrJZ0xZwiM0K1jvFAWB1c8pQL7W9mQs='
+cipher_suite = Fernet(key)
+
+account_address  = '0x46C3d6d846Ada43774999Fc066211BC961b19F70'
+private_key = '0f52b49df6f1d8c737dad58969e6b66bc1ee53f8e2f314b19eb7a4cddd9e9000'
+
+web3 = Web3(Web3.HTTPProvider('https://goerli.infura.io/v3/8600c337513c4cbc8e8d3d9f1f577598'))
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)      # Ignore warning for now
+
+#Hardware SPI configuration:
+SPI_PORT   = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+
+conadress = "0x408449Ae12bF1f3154b422f56f9BEBB04B6B1b9d"
+conabi = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "newMessage",
+				"type": "string"
+			}
+		],
+		"name": "addMessage",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getAllMessages",
+		"outputs": [
+			{
+				"internalType": "string[]",
+				"name": "",
+				"type": "string[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getLastMessage",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "index",
+				"type": "uint256"
+			}
+		],
+		"name": "getMessage",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "index",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "newMessage",
+				"type": "string"
+			}
+		],
+		"name": "setMessage",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
+
+if __name__ == '__main__':
+    LCD.init_lcd()
+    time.sleep(1)
+
+    p = PulseSensor()
+    p.startAsyncBPM()
+
+    #value = mcp.read_adc(0)
+    #print("value of ECG: " + str(value))
+    contra = web3.eth.contract(address=conadress, abi=conabi)
+    msglst = []
+    while True:
+        bpm = int(p.BPM)
+        value = mcp.read_adc(0)
+        print("value of ECG: " + str(value))
+        if(bpm-125) > 0:
+            #GPIO.output(12, GPIO.HIGH)
+
+            LCD.print_lcd(1, 1, str(bpm - 125))
+            print("HIGH, " + str(bpm - 125))
+            msglst.append((bpm - 125))
+            print(f"data lenth {len(msglst)}")
+            if len(msglst) > 20:
+                msg = Message("ORU_R01")
+                msg.msh.msh_9 = "ORU^R01"
+                msg.add_segment("OBR")
+                msg.obr.obr_4 = str(msglst)  # 假设您的时序数据作为一个字符串存储在此处
+                
+                ecg_message = cipher_suite.decrypt(msg.to_er7()).decode().decode()
+                txn = contra.functions.addMessage(ecg_message).build_transaction({
+                    'from': account_address,
+                    'nonce': web3.eth.get_transaction_count(account_address),
+                    'gas': 2000000,  # 设置足够的 Gas 上限
+                    'gasPrice': web3.eth.gas_price
+                })
+                signed_txn = web3.eth.account.sign_transaction(txn, private_key=private_key)
+                txn_hash = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
+                web3.eth.wait_for_transaction_receipt(txn_hash)
+                print("txn_hash.hex()")
+                
+        else:
+            print("no heart beat")
+        
+        #now = time.strftime('%m/%d %H:%M:%S', time.localtime(time.time()))
+        #LCD.print_lcd(1, 1, 'Hello, world!')
+        time.sleep(0.2)
