531e01e30d2e4b987da5a58ed36f2b4d830a54c8
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2023-12-14 00:14:46 +0800

    backup

--- code/arma.py
+++ code/arma.py
@@ -2,16 +2,35 @@
 import pre_interface as pre
 import pandas as pd
 
+def perform_arma_prediction(data):
+    """
+    Perform ARMA prediction on the given data.
 
+    Parameters:
+    - data (list): A list containing the time series data.
+    - p (int): The order of the autoregressive (AR) component.
+    - q (int): The order of the moving average (MA) component.
+    - k (int): The number of steps to predict.
+
+    Returns:
+    - arma_prediction (list): The predicted values.
+    """
+    global ARMA_pre
+    data = data
+
+    # Perform ARMA prediction using the pre_interface module
+    ARMA_pre, ARMA_pre_inv = pre.interface_pre(data, 3, 15, 1, 20)
+
+    print('Predicted data:', ARMA_pre)
+    
+    return ARMA_pre
+
+    # Example usage:
+    # data = [75, 72, 70, 68, 75, 80, 82, 78, 80, 76]
+
+'''
 global ARMA_pre, data
-data = [75, 72, 70, 68, 75, 80, 82, 78, 80, 76, 78, 75, 80, 85,
-       88, 92, 88, 85, 78, 80, 85, 82, 78, 75, 80, 78, 85, 90,
-       95, 92, 90, 88, 85, 82, 80, 78, 75, 80, 82, 88, 90, 92,
-       95, 100, 98, 96, 92, 88, 85, 82, 80, 78, 75, 80, 78, 85,
-       90, 95, 92, 90, 88, 85, 82, 80, 78, 75, 80, 82, 88, 90,
-       92, 95, 100, 98, 96, 92, 88, 85, 82, 80, 78, 75, 80, 78,
-       85, 90, 95, 92, 90, 88, 85, 82, 80, 78, 75, 80, 82, 88,
-       90, 92, 95, 100, 98, 96, 92, 88, 85, 82, 80, 78, 75]
+data = [75, 72, 70, 68, 75, 80, 82, 78, 80, 76]
 
 
 # 预测(dta,model,p,q=1,k=39)
@@ -19,7 +38,6 @@ print('the len of data:', len(data))
 ARMA_pre, ARMA_pre_inv = pre.interface_pre(data, 3, 15, 1, 20)
 print('predict data:', ARMA_pre)
 
-
-
+'''
 
 #python3 -u "/Users/tanqianqian/Desktop/FinalYearProject/code/arma.py"
--- code/tempCodeRunnerFile.py
+++ code/tempCodeRunnerFile.py
@@ -1,1 +1,7 @@
-rt Web3
+from arma import perform_arma_prediction
+
+data = [11, 23, 32, 43, 54, 65, 76, 87, 98, 102]
+
+
+print(perform_arma_prediction(data))
+#python3 code/tempCodeRunnerFile.py
